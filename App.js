daee7e31271a422015b2164d658fe7bb91ef9587
SahasraRReddy <114499019+SahasraRReddy@users.noreply.github.com>
2023-12-03 08:44:52 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react'
+import {View} from 'react-native'
+import HomeScreen from './screens/HomeScreen'
+
+export default class App extends React.Component{
+  render(){
+
+    return(
+      <View>
+        <HomeScreen/>
+      </View>
+      
+    )
+  }
+}
